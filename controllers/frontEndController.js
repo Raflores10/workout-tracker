91bc21a047258eb41758e940f0318322bafdda61
Raflores10 <120347604+Raflores10@users.noreply.github.com>
2023-02-13 09:46:48 -0800

    update

--- controllers/frontEndController.js
+++ controllers/frontEndController.js
@@ -12,5 +12,14 @@ router.get('/signup', (req, res)=> {
 router.get('/homepage', (req, res)=> {
     res.render("homepage");
 })
+router.get('/squats', (req, res)=> {
+    res.render("squats");
+})
+router.get('/deadlift', (req, res)=> {
+    res.render("deadlift");
+})
+router.get('/benchpress', (req, res)=> {
+    res.render("benchpress");
+})
 
 module.exports = router;
