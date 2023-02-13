c32b19de9113fc5ac1f96d994f7632e686958acc
Derek Caldwell <djohncaldwell@gmail.com>
2023-02-13 06:21:31 -0800

    table data populated on record page

--- controllers/frontEndController.js
+++ controllers/frontEndController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { Workout } = require("../models");
 
 router.get('/login', (req, res)=> {
     res.render("login");
@@ -11,6 +12,20 @@ router.get('/signup', (req, res)=> {
 
 router.get('/homepage', (req, res)=> {
     res.render("homepage");
+})
+
+router.get('/record', (req, res)=> {
+    Workout.findAll({
+        where: {
+            user_id: 1
+        }
+    }).then(workoutData=>{
+        const hbsWorkouts = workoutData.map(workout=>workout.toJSON())
+        res.render("record", {
+            allWorkouts: hbsWorkouts
+        });
+    })
+
 })
 
 module.exports = router;
